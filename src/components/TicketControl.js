c9656d35939c2e29ded79d2d162691e4758972cf
Spencer Moody <spencer.moody@outlook.com>
2020-09-10 11:27:02 -0700

    Add components to app
    Co-authored-by: NoelKirkland <noelkirkland@gmail.com>

---
+++ src/components/TicketControl.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+
+class TicketControl extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  render(){
+    let currentlyVisibleState = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm />
+    } else {
+      currentlyVisibleState = <TicketList />
+    }  
+    return(
+      <React.Fragment>
+        {currentlyVisibleState}
+      </React.Fragment>
+    );
+  }
+}
+
+export default TicketControl;
