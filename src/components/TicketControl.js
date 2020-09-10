f2bfb692ed40f923dcc7a006b9c5f2bfe9e2a455
Spencer Moody <spencer.moody@outlook.com>
2020-09-10 15:34:26 -0700

    Lots of stuff

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import NewTicketForm from './NewTicketForm';
 import TicketList from './TicketList';
+import TicketDetail from './TicketDetail';
 
 class TicketControl extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      masterTicketList: [],
+      selectedTicket: null
     };
+  }  
+
+  handleChangingSelectedTicket = (id) => {
+    const selectedTicket = this.this.state.masterTicketList.filter(ticket => ticket.id === id)[0];
+    this.setState({selectedTicket: selectedTicket});
+  }
+    
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMasterTicketList = this.state.masterTicketList.concat(newTicket);
+    this.setState({masterTicketList: newMasterTicketList, formVisibleOnPage: false });
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }))
   }
 
   render(){
     let currentlyVisibleState = null;
-    if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />
+    let buttonText = null;
+    if (this.state.selectedTicket != null) {
+      currentlyVisibleState = <TicketDetail ticket = {this.state.selectedTicket} />
+      buttonText = "Return to Ticket List";
+    }
+    else if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList}  />;
+      buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList />
-    }  
+      currentlyVisibleState = <TicketList ticketList={this.state.masterTicketList} onTicketSelection={this.handleChangingSelectedTicket} />;
+      buttonText = "Add Ticket";
+    }
     return(
       <React.Fragment>
         {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
--- src/components/TicketList.js
+++ src/components/TicketList.js
@@ -1,36 +1,26 @@
 import React from "react";
 import Ticket from "./Ticket";
+import PropTypes from "prop-types";
 
-const masterTicketList = [
-  {
-    names: 'Thato and Haley',
-    location: '3A',
-    issue: 'Firebase won\'t save record. Halp.'
-  },
-  {
-    names: 'Sleater and Kinney',
-    location: '4B',
-    issue: 'Prop types are throwing an error.'
-  },
-  {
-    names: 'Imani & Jacob',
-    location: '9F',
-    issue: 'Child component isn\'t rendering.'
-  }
-];
-
-function TicketList(){
+function TicketList(props){
   return (
     <React.Fragment>
       <hr/>
-      {masterTicketList.map((ticket, index) =>
-        <Ticket names={ticket.names}
-          location={ticket.location}
-          issue={ticket.issue}
-          key={index}/>
+      {props.ticketList.map((ticket) =>
+        <Ticket
+        whenTicketClicked = {props.onTicketSelection}
+        names={ticket.names}
+        location={ticket.location}
+        issue={ticket.issue}
+        id={ticket.id}
+        key={ticket.id}/>
       )}
     </React.Fragment>
   );
 }
+
+TicketList.propTypes = {
+  ticketList: PropTypes.array
+};
 
 export default TicketList;
